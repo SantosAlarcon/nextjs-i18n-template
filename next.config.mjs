fd19a81adad0ae2c3882b8fc04f3da34bc4eec06
Santos Alarcón <santosalarcon86@gmail.com>
2024-05-27 12:38:24 +0200

    change: imported i18n from i18n config to nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
+import i18nConfig from "./next-i18next.config.js";
+
+const {i18n} = i18nConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-    i18n: {
-		locales: ["en","es","ca"],
-		defaultLocale: "en",
-    },
-    output: "standalone",
+	reactStrictMode: true,
+	i18n,
 };
 
 export default nextConfig;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import {useTranslation} from "next-i18next";
+import { useTranslation } from "next-i18next";
 
 export default function Document() {
-    const {i18n} = useTranslation("common")
-  return (
-    <Html lang={i18n.language}>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	const { i18n } = useTranslation("common");
+	return (
+		<Html lang={i18n.language}>
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
