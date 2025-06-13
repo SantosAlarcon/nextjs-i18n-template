1502dd95805d5a3a881f30f963ac4c8ffe391e43
Santos Alarcón <santosalarcon86@gmail.com>
2025-06-13 12:16:03 +0200

    Updated dependencies and migrate to async params

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -2,97 +2,100 @@ import Image from "next/image";
 import styles from "@/styles/page.module.css";
 import InitTranslations from "@/i18n";
 
-export default async function Home({ params: { lang } }: { params: { lang: string } }) {
-	const { t } = await InitTranslations(lang, ["common"]);
+export default async function Home({
+    params,
+}: { params: Promise<{ lang: string }> }) {
+    const { lang } = await params;
+    const { t } = await InitTranslations(lang, ["common"]);
 
-	return (
-		<div className={styles.page}>
-			<main className={styles.main}>
-				<Image
-					className={styles.logo}
-					src="/next.svg"
-					alt="Next.js logo"
-					width={180}
-					height={38}
-					priority
-				/>
-				<ol>
-					<li>
-						{t("step-1")} <code>app/page.tsx</code>.
-					</li>
-					<li>{t("step-2")}</li>
-				</ol>
+    return (
+        <div className={styles.page}>
+            <main className={styles.main}>
+                <Image
+                    className={styles.logo}
+                    src="/next.svg"
+                    alt="Next.js logo"
+                    width={180}
+                    height={38}
+                    priority
+                />
+                <ol>
+                    <li>
+                        {t("step-1")} <code>app/page.tsx</code>.
+                    </li>
+                    <li>{t("step-2")}</li>
+                </ol>
 
-				<div className={styles.ctas}>
-					<a
-						className={styles.primary}
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<Image
-							className={styles.logo}
-							src="/vercel.svg"
-							alt="Vercel logomark"
-							width={20}
-							height={20}
-						/>
-						{t("deploy-now")}
-					</a>
-					<a
-						href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer"
-						className={styles.secondary}
-					>
-						{t("read-docs")}
-					</a>
-				</div>
-			</main>
-			<footer className={styles.footer}>
-				<a
-					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Image
-						aria-hidden
-						src="/file-text.svg"
-						alt="File icon"
-						width={16}
-						height={16}
-					/>
-					{t("learn")}
-				</a>
-				<a
-					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Image
-						aria-hidden
-						src="/window.svg"
-						alt="Window icon"
-						width={16}
-						height={16}
-					/>
-					{t("examples")}
-				</a>
-				<a
-					href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Image
-						aria-hidden
-						src="/globe.svg"
-						alt="Globe icon"
-						width={16}
-						height={16}
-					/>
-					{t("go-to-nextjs")} →
-				</a>
-			</footer>
-		</div>
-	);
+                <div className={styles.ctas}>
+                    <a
+                        className={styles.primary}
+                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <Image
+                            className={styles.logo}
+                            src="/vercel.svg"
+                            alt="Vercel logomark"
+                            width={20}
+                            height={20}
+                        />
+                        {t("deploy-now")}
+                    </a>
+                    <a
+                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={styles.secondary}
+                    >
+                        {t("read-docs")}
+                    </a>
+                </div>
+            </main>
+            <footer className={styles.footer}>
+                <a
+                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="/file-text.svg"
+                        alt="File icon"
+                        width={16}
+                        height={16}
+                    />
+                    {t("learn")}
+                </a>
+                <a
+                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="/window.svg"
+                        alt="Window icon"
+                        width={16}
+                        height={16}
+                    />
+                    {t("examples")}
+                </a>
+                <a
+                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="/globe.svg"
+                        alt="Globe icon"
+                        width={16}
+                        height={16}
+                    />
+                    {t("go-to-nextjs")} →
+                </a>
+            </footer>
+        </div>
+    );
 }
